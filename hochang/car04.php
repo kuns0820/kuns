f7434c791180cfd73e56ce573efd8c1f4778ff9c
kuns0820 <sf36@naver.com>
2019-07-26 18:39:07 +0900

    no message

---
+++ hochang/car04.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0. user-scalable=no">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/front.css">
+	<link rel="stylesheet" type="text/css" href="css/swiper.min.css">
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/swiper.min.js"></script>
+	<script type="text/javascript" src="js/inc.ui.js"></script>
+</head>
+<body>
+<div id="hc_warp" class="sub">
+	<div id="container">
+		<div class="visual car"><div class="innr">
+			<h2>제품소개</h2>
+			<p>자동차부품사업부<br />㈜호창MF</p>
+		</div></div>
+		<div class="bread"><div class="innr">
+			<a href="main.php" class="home">홈</a>
+			<div class="deps">
+				<a href="javascript:;" class="selected">자동차부품사업부</a>
+				<ul class="layer_deps">
+					<li><a href="company01.php">호창 HOCHANG</a></li>
+					<li><a href="car01.php">자동차부품사업부</a></li>
+					<li><a href="build01.php">건설자재사업부</a></li>
+					<li><a href="bio01.php">바이오사업부</a></li>
+				</ul>
+			</div>
+			<div class="deps dep2">
+				<a href="javascript:;" class="selected">제품소개</a>
+				<ul class="layer_deps">
+					<li><a href="car01.php">제조공법 흐름도</a></li>
+					<li><a href="car02.php">제조공정</a></li>
+					<li><a href="car03.php">설비&측정 설비현황</a></li>
+					<li><a href="car04.php">제품소개</a></li>
+					<li><a href="car05.php">R&D 현황</a></li>
+				</ul>
+			</div>
+		</div></div>
+		<article class="">
+			<img src="images/sub/car04.jpg" alt="" class="pc">
+			<img src="images/sub/m_car04.jpg" alt="" class="mo">
+		</article>
+	</div>
+</div>
+</body>
+</html>
---
+++ hochang/sitemap.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0. user-scalable=no">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/front.css">
+	<link rel="stylesheet" type="text/css" href="css/swiper.min.css">
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/swiper.min.js"></script>
+	<script type="text/javascript" src="js/inc.ui.js"></script>
+</head>
+<body>
+<div id="hc_warp" class="sub">
+	<div id="container">
+		<div class="visual company"><div class="innr">
+			<h2>인사말</h2>
+			<p>호창MF를 방문해 주셔서 감사합니다.<br />신뢰를 바탕으로 최고의 비지니스 파트너가 되겠습니다.</p>
+		</div></div>
+		<div class="bread map"><div class="innr">
+			<a href="main.php" class="home">홈</a>
+			<div class="deps">
+				<a href="javascript:;" class="selected">사이트맵</a>
+			</div>
+		</div></div>
+		<article class="sitemap">
+			<dl>
+				<dt><div><p>호창 HOCHANG</p></div></dt>
+				<dd><a href="company01.php">인사말</a></dd>
+				<dd><a href="company02.php">연혁</a></dd>
+				<dd><a href="company03.php">조직도</a></dd>
+				<dd><a href="company04.php">인증현황&특허</a></dd>
+				<dd><a href="company05.php">오시는길</a></dd>
+			</dl>
+			<dl>
+				<dt><div>
+					<p>자동차부품사업부</p>
+					<span>㈜호창MF</span>
+				</div></dt>
+				<dd><a href="car01.php">제조공법 흐름도</a></dd>
+				<dd><a href="car02.php">제조공정</a></dd>
+				<dd><a href="car03.php">설비&측정 설비현황</a></dd>
+				<dd><a href="car04.php">제품소개</a></dd>
+				<dd><a href="car05.php">R&D 현황</a></dd>
+			</dl>
+			<dl>
+				<dt><div>
+					<p>건설자재사업부</p>
+					<span>㈜호창메탈</span>
+				</div></dt>
+				<dd><a href="build01.php">제품소개</a></dd>
+				<dd><a href="build02.php">시험성적서</a></dd>
+				<dd><a href="build03.php">동영상</a></dd>
+				<dd><a href="build04.php">견적문의</a></dd>
+				<dd><a href="build05.php">기술자료</a></dd>
+			</dl>
+			<dl>
+				<dt><div>
+					<p>바이오사업부</p>
+					<span>㈜호창메탈</span>
+				</div></dt>
+				<dd><a href="bio01.php">Strategy</a></dd>
+				<dd><a href="bio02.php">R&D</a></dd>
+				<dd class="sub"><a href="bio02.php">Biotechnology</a></dd>
+				<dd class="sub"><a href="bio03.php">Biocosmetics</a></dd>
+				<dd><a href="bio04.php">Products</a></dd>
+			</dl>
+		</article>
+	</div>
+</div>
+</body>
+</html>
