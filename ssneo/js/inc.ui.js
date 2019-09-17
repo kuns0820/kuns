fa05817ec8b4e70874c0ca715b7a1b84e48f1431
kuns0820 <kuns0820@gmail.com>
2019-09-17 21:23:44 +0900

    no message

--- ssneo/js/inc.ui.js
+++ ssneo/js/inc.ui.js
@@ -5,11 +5,32 @@ var inc_dom = {
 		incHD.push('<h1><a href=""><img src="images/logo.png" alt="ABIT"></a></h1>');
 		incHD.push('<div class="gnb">');
 		incHD.push('<ul>');
-		incHD.push('<li><a href="">IEO</a></li>');
-		incHD.push('<li><a href="">자산현황</a></li>');
-		incHD.push('<li><a href="">커뮤니티</a></li>');
-		incHD.push('<li><a href="">고객지원</a></li>');
-		incHD.push('<li><a href="">제휴&middot;제안</a></li>');
+		incHD.push('<li><a href=""><span>IEO</span></a>');
+		incHD.push('<ul class="dep2">');
+		incHD.push('<li><a href="">IEO 참여신청</a></li>');
+		incHD.push('<li><a href="">신청 내역리스트<br />확인</a></li>');
+		incHD.push('</ul>');
+		incHD.push('</li>');
+		incHD.push('<li><a href=""><span>자산현황</span></a></li>');
+		incHD.push('<li><a href=""><span>커뮤니티</span></a>');
+		incHD.push('<ul class="dep2">');
+		incHD.push('<li><a href="">뉴스</a></li>');
+		incHD.push('<li><a href="">NEO insight</a></li>');
+		incHD.push('<li><a href="">이벤트</a></li>');
+		incHD.push('</ul>');
+		incHD.push('</li>');
+		incHD.push('<li><a href=""><span>고객지원</span></a>');
+		incHD.push('<ul class="dep2">');
+		incHD.push('<li><a href="">공지사항</a></li>');
+		incHD.push('<li><a href="">FAQ</a></li>');
+		incHD.push('</ul>');
+		incHD.push('</li>');
+		incHD.push('<li><a href=""><span>제휴&middot;제안</span></a>');
+		incHD.push('<ul class="dep2">');
+		incHD.push('<li><a href="">사업 제휴</a></li>');
+		incHD.push('<li><a href="">상장</a></li>');
+		incHD.push('</ul>');
+		incHD.push('</li>');
 		incHD.push('</ul>');
 		incHD.push('</div>');
 		incHD.push('<div class="util">');
@@ -47,7 +68,7 @@ var inc_dom = {
 }
 
 $(function(){
-//	inc_dom.header();
+	inc_dom.header();
 	inc_dom.footer();
 });
 
@@ -64,9 +85,15 @@ function layersClose(target){
 
 $(function(){
 	$(".gnb li").on("mouseover" , function(){
-		$(".gnb li .dep2").show();
+		$(".gnb li .dep2").css({
+			"opacity" : "1",
+			"visibility" : "visible"
+		});
 	});
 	$(".gnb li").on("mouseout" , function(){
-		$(".gnb li .dep2").hide();
+		$(".gnb li .dep2").css({
+			"opacity" : "0",
+			"visibility" : "hidden"
+		});
 	});
 });
