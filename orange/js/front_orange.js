bde75726fdf331d84751ac1adceda1062920bbd2
kuns0820 <kuns0820@gmail.com>
2020-03-18 20:10:10 +0900

    no message

---
+++ orange/js/front_orange.js
@@ -0,0 +1,44 @@
+$(function(){
+	var _body = $("body");
+	$("header .btn-menu").click(function(){
+		if(!_body.hasClass("menuOn")){
+			_body.addClass("menuOn");
+		}else{
+			_body.removeClass("menuOn");
+		}
+	});
+	$("#side_wrap .menu_close").click(function(){
+		_body.removeClass("menuOn");
+	});
+
+	$("#side_wrap .menu_dep1 li a").click(function(){
+		_target = $(this).parent().data("tg");
+		$(this).parent().addClass("on").siblings().removeClass("on");
+		$("#side_wrap .menu_dep2 ul." + _target).addClass("on").siblings().removeClass("on");
+	});
+});
+
+function layers(target){
+	$this = $(target).attr("data-layer");
+	$dom = $(".layer-wrap .layer-conts."+ $this);
+	$dom.closest(".layer-wrap").fadeIn("fast");
+	$dom.fadeIn("fast");
+
+	var _docT = $(document).height();
+	var _winT = $(window).scrollTop() + 30;
+	var _layerT = $dom.height();
+
+	if(_winT + _layerT >= _docT){
+		$dom.css({
+			top : _winT - ((_winT + _layerT) - _docT + 50)
+		});
+	}else{
+		$dom.css({
+			top : _winT
+		});
+	}
+}
+function layersClose(target){
+	$this = $(target).closest(".layer-wrap");
+	$this.fadeOut("fast");
+}
